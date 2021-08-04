eb440541d9941209a6e5b1ff4de5d9d754e51492
kahil nayton <kahilnayton@gmail.com>
2021-08-04 19:35:24 -0400

    added linting

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,18 @@
-/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.jest.json",
+    },
+  },
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
